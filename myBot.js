a0ad770caf9dd813fe14800e34d456aaec427434
abwizard <terapea@hotmail.com>
2017-05-14 22:25:02 +0900

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -1,13 +1,13 @@
-﻿const Discord = require('discord.js'); //เรียก discord.js มาใช้
+const Discord = require('discord.js'); //เรียก discord.js มาใช้
 const botAst = new Discord.Client(); //ประกาศ client ขึ้นมา
 //event นี้ทำงานเมื่อ login สำเร็จ
 botAst.on('ready', () => {
   console.log('Astolfo ready!');
 });
 //รอรับ event message เวลามีข้อความโผล่มาในแชท function นี้ก็จะทำงาน
-botRem.on('message', message => { 
+botAst.on('message', message => { 
   if (message.content === 'แอสโทลโฟน่ารักจังเลยน้า') {
     message.reply('ขอบคุณนะ มาสเตอร์');
   }
 });
-botRem.login('YOUR BOT TOKEN HERE!!!');
+botAst.login('YOUR BOT TOKEN HERE!!!');
